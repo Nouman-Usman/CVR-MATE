a356c184d01c9f1929d70772d66ce8d63361235e
Nouman-Usman <2022CS49@student.uet.edu.pk>
2026-07-14 12:46:51 +0500

    feat(crm): remove team feature from coming soon section and related components

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,8 +2,7 @@ export const CONTACT_EMAIL = "dev@fourmates.dk"
 
 // Remove a key here to "turn on" the feature when it's ready
 export const COMING_SOON_FEATURES = new Set([
-  "team",
   "crm",
 ] as const)
 
-export type ComingSoonFeature = "team" | "crm"
+export type ComingSoonFeature = "crm"
